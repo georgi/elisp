55df93d1023adaca1b69866da65f498657da99f9
Matthias Georgi <matti.georgi@gmail.com>
2010-10-26 23:00:08 +0200

    make win32 compatible

--- init.el
+++ init.el
@@ -36,20 +36,6 @@
 (setq tab-width 4)
 (setq tooltip-delay 10)
 
-;; ********************************************************************************
-;; CEDET.
-(load-file "~/.emacs.d/cedet/common/cedet.el")
-
-(global-ede-mode 1)
-
-;; ********************************************************************************
-;; Semantic
-(semantic-load-enable-minimum-features)
-;; (semantic-load-enable-code-helpers)
-;; (semantic-load-enable-gaudy-code-helpers)
-;; (semantic-load-enable-all-exuberent-ctags-support)
-
-
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/yasnippet/snippets")
@@ -69,68 +55,8 @@
 ;; (add-to-list 'load-path (concat rsense-home "/etc"))
 ;; (require 'rsense)
 
-(require 'ecb)
-(setq ecb-primary-secondary-mouse-buttons 'mouse-1--mouse-2)
-(setq ecb-history-make-buckets 'mode)
-(setq ecb-add-path-for-not-matching-files (quote (nil)))
-(setq ecb-auto-activate nil)
-(setq ecb-clear-caches-before-activate t)
-(setq ecb-compile-window-height 20)
-(setq ecb-options-version "2.40")
-(setq ecb-tip-of-the-day nil)
-(setq ecb-toggle-layout-sequence '("top1" "left2" "leftright2"))
-
-;; (setq ecb-vc-supported-backends (quote ((ecb-vc-dir-managed-by-SVN . ecb-vc-state) (ecb-vc-dir-managed-by-GIT . ecb-vc-state))))
-
-(setq ecb-common-directories-menu
-      '((ecb-grep-directory "Grep Directory")
-        (ecb-grep-find-directory "Grep Directory recursive")
-        (ecb-dired-directory "Open in Dired")
-        (ecb-dired-directory-other-window "Open in Dired other window")
-        ("---")
-        (ecb-create-source "Create Sourcefile")
-        (ecb-create-directory "Create Child Directory")
-        (ecb-delete-directory "Delete Directory")
-        ("---")
-        (ecb-add-source-path-node "Add Source Path")))
-
-(setq ecb-directories-menu ecb-common-directories-menu)
-
-
-(setq ecb-history-menu
-      (append
-      '((ecb-grep-directory "Grep Directory")
-        (ecb-grep-find-directory "Grep Directory recursive")
-        (ecb-dired-directory "Open in Dired")
-        (ecb-dired-directory-other-window "Open in Dired other window")
-        ("---")
-         ("Filter"
-          (ecb-popup-history-filter-by-ext "Filter by extension")
-          (ecb-popup-history-filter-by-regexp "Filter by regexp")
-          (ecb-popup-history-filter-all-existing "No filter"))
-         ("---")
-         (ecb-history-kill-buffer "Kill Buffer")
-         (ecb-delete-source "Delete Sourcefile"))
-       ecb-history-common-menu))
-
-
-(setq ecb-sources-menu
-      '((ecb-grep-directory "Grep Directory")
-        (ecb-grep-find-directory "Grep Directory recursive")
-        (ecb-dired-directory "Open in Dired")
-        (ecb-dired-directory-other-window "Open in Dired other window")
-        ("Filter"
-         (ecb-popup-sources-filter-by-ext "Filter by extension")
-         (ecb-popup-sources-filter-by-regexp "Filter by a regexp")
-         (ecb-popup-sources-filter-none "No filter"))
-        ("---")        
-        (ecb-create-source "Create Sourcefile")
-        (ecb-delete-source "Delete Sourcefile")
-        ("---")
-        (ecb-maximize-ecb-window-menu-wrapper "Maximize window")))
-
-
-
+(if (not (eq system-type 'windows-nt))
+    (require 'ecb-init))
 
 (require 'browse-kill-ring)
 (browse-kill-ring-default-keybindings)
@@ -883,3 +809,13 @@
 (global-set-key (kbd "C-=") 'increase-font-size)
 
 (set-face-attribute 'default nil :height current-font-size)
+
+(require 'server)
+
+(when (and (= emacs-major-version 23)
+           (= emacs-minor-version 1)
+           (equal window-system 'w32))
+  (defun server-ensure-safe-dir (dir) "Noop" t)) ; Suppress error "directory
+                                                 ; ~/.emacs.d/server is unsafe"
+                                                 ; on windows.
+(server-start)
