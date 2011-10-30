e57d2e3c4c6644221a86a0849c373f6338a2d885
Matthias Georgi <matti.georgi@gmail.com>
2011-10-30 16:07:28 +0100

    added evil

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 (add-to-list 'load-path "~/.emacs.d/")
 (add-to-list 'load-path "~/.emacs.d/apel")
 (add-to-list 'load-path "~/.emacs.d/erlang")
+(add-to-list 'load-path "~/.emacs.d/evil")
 (add-to-list 'load-path "~/.emacs.d/js2-mode")
 (add-to-list 'load-path "~/.emacs.d/nxml-mode")
 (add-to-list 'load-path "~/.emacs.d/rhtml")
@@ -37,7 +38,7 @@
   (require 'tabbar))
 
 (setenv "PATH"
-        (shell-command-to-string "source $HOME/.zshrc>/dev/null && printf $PATH"))
+        (concat (shell-command-to-string "source $HOME/.zshrc>/dev/null && printf $PATH")))
 
 ;; ********************************************************************************
 ;; Variables
@@ -72,6 +73,9 @@
 (setq ac-auto-start nil)
 
 (global-auto-complete-mode t)
+
+(require 'evil)
+(evil-mode 1)
 
 ;; ********************************************************************************
 ;; Defuns
@@ -349,24 +353,18 @@
 (defun rspec-run-single-file (spec-file &rest opts)
   "Runs spec with the specified options"
   (rspec-register-verify-redo (cons 'rspec-run-single-file (cons spec-file opts)))
-  (compile (concat "spec " spec-file " --drb " (mapconcat (lambda (x) x) opts " ")))
+  (compile (concat "~/.emacs.d/spec " spec-file " --drb " (mapconcat (lambda (x) x) opts " ")))
   (end-of-buffer-other-window 0))
 
 (defun rspec-verify ()
   "Runs the specified spec, or the spec file for the current buffer."
   (interactive)
-  (rspec-run-single-file (rspec-spec-file-for (buffer-file-name))))
+  (rspec-run-single-file (rspec-spec-file-for (buffer-file-name)) "--format" "progress"))
 
 (defun rspec-verify-single ()
   "Runs the specified example at the point of the current buffer."
   (interactive)
   (rspec-run-single-file (rspec-spec-file-for (buffer-file-name)) (concat "--line " (number-to-string (line-number-at-pos)))))
-
-(defun rspec-verify-all ()
-  "Runs the 'spec' rake task for the project of the current file."
-  (interactive)
-  (let ((default-directory (or (rspec-project-root) default-directory)))
-    (rspec-run "--format=progress")))
 
 
 
